5ec6f4ac0b09f651a48edeaceabe80370eaf7bf5
taiki-mtb <68456089+taiki-mtb@users.noreply.github.com>
2021-07-04 15:53:08 +0900

    追加ボタンを押すと未完了のtodoに移動

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいい",
     "uuuu"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="Todoを入力" />
-        <button>追加</button>.
+        <input
+          placeholder="Todoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>.
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTodo</p>
